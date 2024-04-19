32d25a890b3a2a844e746e5a30c310338806ad20
ericdwkim <ericdwkim.io@gmail.com>
2024-04-18 22:53:26 -0500

    wip: `resend` & contact form component

---
+++ actions/sendEmail.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import React from 'react';
+import { Resend } from 'resend';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from '../../components/EmailTemplate';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['delivered@resend.dev'],
+        subject: 'Hello world',
+        react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+        return res.status(400).json(error);
+    }
+
+    res.status(200).json(data);
+};
