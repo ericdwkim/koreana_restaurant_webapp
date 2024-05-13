342bd530267bd6843515a327f9ed8f2aef43c51b
ericdwkim <ericdwkim.io@gmail.com>
2024-05-13 16:51:24 -0500

    fixed: navbar navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,28 +3,24 @@
 import React, { useState } from "react";
 import Image from 'next/image';
 import Link from "next/link";
-import { useRouter } from "next/router"; // Import useRouter
+import { usePathname } from "next/navigation"; // Import useRouter
 import logo from "../public/logo.png";
 import { navbarLinks } from "@/lib/links";
 
 export const Navbar = () => {
-  const router = useRouter(); // Use useRouter instead of useLocation
+  const pathname = usePathname();
   const [isActiveMobile, setIsActiveMobile] = useState(false);
 
   return (
     <nav className="bg-white shadow-md py-4 px-5 flex justify-between items-center">
-      <Link href="/">
-        <a>
-          <div className="flex items-center">
-            <Image 
-              src={logo} 
-              alt="Website logo" 
-              className="h-10" 
-              width={100}
-              height={100}
-            />
-          </div>
-        </a>
+      <Link href="/" className="flex items-center">
+        <Image 
+          src={logo} 
+          alt="Website logo" 
+          className="h-10" 
+          width={100}
+          height={100}
+        />
       </Link>
       <svg
         onClick={() => setIsActiveMobile(!isActiveMobile)}
@@ -43,10 +39,8 @@ export const Navbar = () => {
       </svg>
       <div className={`md:flex hidden ${isActiveMobile ? "flex" : "hidden"} flex-col md:flex-row items-center`}>
         {navbarLinks.map(link => (
-          <Link key={link.href} href={link.href}>
-            <a className={`px-3 py-2 rounded ${router.pathname === link.href ? "bg-blue-500 text-white" : "text-gray-700"}`}>
-              {link.label}
-            </a>
+          <Link key={link.href} href={link.href} className={`px-3 py-2 rounded ${pathname === link.href ? "bg-blue-500 text-white" : "text-gray-700"}`}>
+            {link.label}
           </Link>
         ))}
       </div>
