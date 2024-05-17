ece9f5722323d4e65c7a3f1d8da820b214be29f1
ericdwkim <ericdwkim.io@gmail.com>
2024-05-17 13:58:46 -0500

    remove redundant nav Link component in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import { HomeContent } from '@/components/HomeContent';
 const Home: NextPage = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-        </ul>
-      </nav>
+
       <HomeContent/>
     </div>
   );
