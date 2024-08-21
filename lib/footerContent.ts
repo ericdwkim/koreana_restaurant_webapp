a30257389612f30630df6f0f0a69b2d0294ca069
ericdwkim <ericdwkim.io@gmail.com>
2024-08-21 16:53:18 -0500

    structured footer content as lib

---
+++ lib/footerContent.ts
@@ -0,0 +1,21 @@
+export const footerContent = [
+    {
+        title: "About Us",
+        content: [
+            "For general comments or questions about catering needs, reservations, ordering, menu items, please call us directly during business hours.",
+            "For website-related questions, please use the contact form.",
+        ],
+    },
+    {
+        title: "Contact Us",
+        content: ["2458 Harry Wurzbach Rd", "San Antonio, TX 78209", "(210)-804-0019"],
+    },
+    {
+        title: "Open Positions",
+        content: ["- Servers", "- Cooks", "- Dishwashers", "Want to join the team or know someone who might? Call or inquire within."],
+    },
+    {
+        title: "Business Hours",
+        content: ["Mon - Thurs 11am - 9pm", "Fri & Sat 11am - 10pm", "Sunday 12pm – 7pm", "*Closed on Wednesdays starting 9/04/24"],
+    },
+];
